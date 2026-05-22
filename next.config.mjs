1b626f9db4d65933741f86b8fcffd5e603f6fb1b
Flyone <1508948470@qq.com>
2026-05-22 14:16:25 +0800

    fix: 添加basePath配置以支持GitHub Pages子路径部署

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   output: 'export',
+  basePath: '/flyono-base',
   reactStrictMode: true,
   images: {
     unoptimized: true,
